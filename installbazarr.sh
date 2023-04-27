68f2e3fc00e5d5b5d8e65c235f08246bef3cc0e9
juanajok <129669388+juanajok@users.noreply.github.com>
2023-04-27 15:24:44 +0200

    ok

--- installbazarr.sh
+++ installbazarr.sh
@@ -1,118 +1,74 @@
-#!/bin/sh
+#!/bin/bash
 
-# Script Name: confiraspi
-# Description: cript con los pasos para reinstalar la raspa
-# Author: Juan José Hipólito
-# Version: 2..0.0
-# Date: 2023-03-30
-# License: GNU
-# Usage: Debes ejecutar el script desde la ruta donde estén los ficheros
-#   dhcpcd.conf
-#   settings.json
-#   smb.conf
-# Dependencies: No tiene
-# Notes: Este script tiene como objetivo configurar una Raspberry Pi con varios servicios y programas, como Samba, XRDP, Transmission, Mono, Sonarr, Webmin, VNC, Plex, Bazarr y aMule. A continuación, se describen brevemente las funciones principales de cada uno de estos servicios y programas:
+usuario="$SUDO_USER"
+app_guid=$(id -gn "$usuario")
 
-#Samba: Permite compartir archivos y directorios a través de una red local.
-#XRDP: Habilita el acceso remoto al escritorio de la Raspberry Pi.
-#Transmission: Cliente de torrent para descargar y compartir archivos.
-#Mono: Entorno de desarrollo para ejecutar aplicaciones basadas en .NET.
-#Sonarr: Gestiona automáticamente tus series de TV y descarga nuevos episodios.
-#Webmin: Herramienta de administración de sistemas basada en web para Linux.
-#VNC: Permite el control remoto gráfico de una Raspberry Pi.
-#Plex: Servidor de medios para organizar y transmitir películas, series de TV y música.
-#Bazarr: Permite la descarga automática de subtítulos para tus series y películas.
-#aMule: Cliente P2P para compartir archivos a través de la red eD2k y Kademlia.
+# Cambiar a usuario root
 
-log() {
-    #función que permitirá que los mensajes de salida identifiquen al script, la función y fecha/hora de ejecución
-    local message="$1"
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$0] [${FUNCNAME[1]}] $message"
-}
+# Actualizar repositorios e instalar paquetes necesarios
+apt-get update
+apt-get install -y libxml2-dev libxslt1-dev python3-dev python3-libxml2 python3-lxml unrar-free ffmpeg libatlas-base-dev -y
 
+# Comprobar versión de Python instalada y actualizar si es necesario
+python_version=$(python3 -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
+if [ "$(echo "$python_version < 3.7" | bc)" -eq 1 ]; then
+  echo "Python $python_version no es compatible, se requiere versión 3.7 o superior. Actualizando Python..."
+  apt-get install -y python3.8
+fi
 
-leer_credenciales() {
-    if [ "$EUID" -ne 0 ]
-    then
-        log "Error: Por favor, ejecuta el script como superusuario (sudo)."
-        exit 1
-    fi
+# Descargar y descomprimir Bazarr
+mkdir -p /opt/bazarr
+wget -P /opt/bazarr https://github.com/morpheus65535/bazarr/releases/latest/download/Bazarr-linux64.zip
+unzip -d /opt/bazarr /opt/bazarr/Bazarr-linux64.zip
+rm /opt/bazarr/Bazarr-linux64.zip
 
-    usuario="$SUDO_USER"
-    app_guid=$(id -gn "$usuario")
-}
+# Instalar requisitos de Python
+python3 -m pip install -r /opt/bazarr/requirements.txt
+
+# En Raspberry Pi antiguas (ARMv6) numpy no es compatible
+if [ "$(uname -m)" = "armv6l" ]; then
+  echo "Raspberry Pi antigua detectada. Reemplazando numpy..."
+  python3 -m pip uninstall -y numpy
+  apt-get install -y python3-numpy
+fi
+
+# Cambiar propiedad de la carpeta de Bazarr al usuario deseado
+chown -R $usuario:$app_guid /opt/bazarr
+chmod -R 777 /opt/bazarr
 
 
-
-instalar_bazarr() {
-
-    leercredenciales
-
-    log "Instalando dependencias de Bazarr..."
-    sudo sudo apt-get install libxml2-dev libxslt1-dev python3-dev python3-libxml2 python3-lxml unrar-free ffmpeg libatlas-base-dev
-
-
-    log "Creando la carpeta de Bazarr..."
-    mkdir -p /opt/bazarr
-    chown -R $usuario:$usuario /opt/bazarr
-    chmod 755 /opt/bazarr
-
-    if [ ! -d "/opt/bazarr/.git" ]; then
-        log "Clonando el repositorio de Bazarr en la carpeta..."
-        git clone https://github.com/morpheus65535/bazarr/releases/latest/download/bazarr.zip /opt/bazarr
-    else
-        log "El repositorio de Bazarr ya está clonado. No es necesario clonarlo de nuevo."
-    fi
-    chmod 777 /opt/bazarr
-
-
-    log "Navegando a la carpeta de Bazarr..."
-    cd /opt/bazarr
-
-    if [ ! -d "venv" ]; then
-        log "Creando el entorno virtual de Python..."
-        python3 -m venv venv
-    else
-        log "El entorno virtual de Python ya existe. No es necesario crearlo de nuevo."
-    fi
-
-    log "Activando el entorno virtual y actualizando las dependencias de Bazarr..."
-    source venv/bin/activate
-    pip install -r requirements.txt
-    deactivate
-
-    if [ ! -f "/etc/systemd/system/bazarr.service" ]; then
-        log "Creando el servicio de Bazarr..."
-        sudo bash -c "cat > /etc/systemd/system/bazarr.service << EOL
+# Crear el archivo de servicio de systemd
+cat << EOF | tee /etc/systemd/system/bazarr.service
 [Unit]
 Description=Bazarr Daemon
 After=syslog.target network.target
 
 [Service]
-User=$usuario
-Group=$app_guid
-UMask=002
+WorkingDirectory=/opt/bazarr/
+User=${usuario}
+Group=${app_guid}
+UMask=0002
+Restart=on-failure
+RestartSec=5
 Type=simple
-ExecStart=/opt/bazarr/venv/bin/python3 /opt/bazarr/bazarr.py
-Restart=always
-RestartSec=15
+ExecStart=/usr/bin/python3 /opt/bazarr/bazarr.py
+KillSignal=SIGINT
+TimeoutStopSec=20
+SyslogIdentifier=bazarr
+ExecStartPre=/bin/sleep 30
 
 [Install]
 WantedBy=multi-user.target
-EOL"
-    else
-        log "El servicio de Bazarr ya existe. No es necesario crearlo de nuevo."
-    fi
+EOF
 
-    log "Habilitando e iniciando el servicio de Bazarr..."
-    sudo systemctl enable bazarr.service
-    sudo systemctl start bazarr.service
-    sudo systemctl status bazarr.service
+# Iniciar y habilitar el servicio
+sudo systemctl start bazarr
+sudo systemctl enable bazarr
+sudo systemctl status bazarr
 
+# Mensaje de confirmación y enlace para acceder a la interfaz web de Bazarr
+echo "¡Bazarr ha sido instalado correctamente!"
+echo "Accede a la interfaz web en http://<raspberry_pi_ip>:6767"
 
-    log "Bazarr instalado. Visita http://<raspberry_pi_ip>:6767 para configurarlo."
-    #volvemos al directorio donde está el script
-    cd "$script_path"
-}
-
-instalar_bazarr
+# Salir del script
+exit 0
