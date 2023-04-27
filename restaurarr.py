a9c3bf5c8fdc96d501266c837c6fb6a62f972b4c
juanajok <129669388+juanajok@users.noreply.github.com>
2023-04-27 23:01:42 +0200

    ok

--- restaurarr.py
+++ restaurarr.py
@@ -7,37 +7,35 @@ import tarfile
 import zipfile
 import stat
 
-# Definir la ruta de los backups y la extensión
-BACKUP_DIR = "/media/Backup/"
-BACKUP_EXT = {
-    "zip": zipfile.ZipFile,
-    "tar": tarfile.open,
-    "": None  # No hay extensión para archivos sin comprimir
-}
+#carga el contenido de un archivo de configuración en formato JSON y lo devuelve como un diccionario de Python.
+def load_config(config_file):
+    with open(config_file) as f:
+        config = json.load(f)
+    return config
 
-# Cargar la configuración desde el archivo JSON
-with open("restore_apps.json") as f:
-    config = json.load(f)
+#devuelve la ruta completa del archivo de backup más reciente de la aplicación
+def get_latest_backup(backup_dir, backup_ext):
+    backup_files = os.listdir(backup_dir)
+    backup_files = [f for f in backup_files if f.endswith(backup_ext)]
+    backup_files = sorted(backup_files, key=lambda f: os.path.getmtime(os.path.join(backup_dir, f)), reverse=True)
+    if len(backup_files) == 0:
+        return None
+    else:
+        return os.path.join(backup_dir, backup_files[0])
 
-# Iterar sobre las aplicaciones y sus configuraciones
-for app, app_config in config.items():
+
+def restore_app(app, app_config):
     backup_dir = os.path.join(BACKUP_DIR, app_config["backup_dir"])
     restore_dir = app_config["restore_dir"]
     backup_ext = app_config["backup_ext"]
     files = app_config.get("files_to_restore", None)  # Lista opcional de ficheros a restaurar
     permissions = app_config.get("permissions", {})
 
-
-    # Encontrar el último backup disponible en el directorio
-    backup_files = os.listdir(backup_dir)
-    backup_files = [f for f in backup_files if f.endswith(backup_ext)]
-    backup_files = sorted(backup_files, reverse=True)
-    if len(backup_files) == 0:
+    backup_file = get_latest_backup(backup_dir, backup_ext)
+    if backup_file is None:
         print(f"No se encontraron backups para la aplicación {app}.")
-        continue
+        return
 
-    # Descomprimir o copiar el backup en la carpeta de restauración
-    backup_file = os.path.join(backup_dir, backup_files[0])
     backup_ext = os.path.splitext(backup_file)[-1]
     if backup_ext in BACKUP_EXT:
         with BACKUP_EXT[backup_ext](backup_file, "r") as f:
@@ -60,6 +58,14 @@ for app, app_config in config.items():
 
     print(f"Aplicación {app} restaurada desde {backup_file}.")
 
-# Ejecuta el script
+
+def main():
+    config_file = "restore_apps.json"
+    config = load_config(config_file)
+
+    for app, app_config in config.items():
+        restore_app(app, app_config)
+
+
 if __name__ == '__main__':
     main()
