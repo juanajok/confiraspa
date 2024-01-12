8668a08e701a702608975fc7efd1f7f11f1a243a
juanajok <129669388+juanajok@users.noreply.github.com>
2024-01-12 13:57:44 +0100

    l

--- nmclistatip.sh
+++ nmclistatip.sh
@@ -13,11 +13,14 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-# Verifica la existencia de la CONEXION
-if ! nmcli device status | grep -q "$CONEXION"; then
-    echo "La CONEXION $CONEXION no existe o no está activa." >&2
+# Verifica la existencia de la conexión
+if ! nmcli con show | grep -q "$CONEXION"; then
+    echo "La conexión $CONEXION no existe." >&2
     exit 1
 fi
+
+# Encuentra el nombre del dispositivo asociado con esta conexión
+NOMBRE_DISPOSITIVO=$(nmcli -t -f DEVICE con show "$CONEXION")
 
 # Función para validar la dirección IP y la máscara de red
 validar_ip_mascara() {
@@ -56,26 +59,30 @@ if ! validar_ip_mascara "$IP_ESTATICA/$MASCARA" || ! validar_gateway "$GATEWAY" 
 fi
 
 # Obtiene la configuración actual
-ACTUAL_IP=$(nmcli -g IP4.ADDRESS dev show $CONEXION)
-ACTUAL_GATEWAY=$(nmcli -g IP4.GATEWAY dev show $CONEXION)
-ACTUAL_DNS=$(nmcli -g IP4.DNS dev show $CONEXION | paste -sd "," -)
+ACTUAL_IP=$(nmcli -g IP4.ADDRESS con show "$CONEXION")
+ACTUAL_GATEWAY=$(nmcli -g IP4.GATEWAY con show "$CONEXION")
+ACTUAL_DNS=$(nmcli -g IP4.DNS con show "$CONEXION" | paste -sd "," -)
 
 # Compara la configuración actual con la deseada
 if [ "$ACTUAL_IP" = "$IP_ESTATICA/$MASCARA" ] && [ "$ACTUAL_GATEWAY" = "$GATEWAY" ] && [ "$ACTUAL_DNS" = "$DNS" ]; then
-    echo "La configuración de red ya está establecida. No se requieren cambios."
+    echo "La configuración de red ya está establecida en $CONEXION. No se requieren cambios."
     exit 0
 fi
 
-# Aplica la configuración
+# Aplica la configuración a la conexión
 if nmcli con mod "$CONEXION" ipv4.addresses "$IP_ESTATICA/$MASCARA" &&
    nmcli con mod "$CONEXION" ipv4.gateway "$GATEWAY" &&
    nmcli con mod "$CONEXION" ipv4.dns "$DNS" &&
    nmcli con mod "$CONEXION" ipv4.method manual; then
 
-    # Reinicia la CONEXION de red para aplicar los cambios
-    nmcli con down "$CONEXION" && nmcli con up "$CONEXION"
-    echo "Configuración de IP estática aplicada a $CONEXION"
+    # Reinicia la conexión de red para aplicar los cambios
+    if [ -n "$NOMBRE_DISPOSITIVO" ]; then
+        nmcli device disconnect "$NOMBRE_DISPOSITIVO" && nmcli device connect "$NOMBRE_DISPOSITIVO"
+    else
+        nmcli con down "$CONEXION" && nmcli con up "$CONEXION"
+    fi
+    echo "Configuración de IP estática aplicada a la conexión $CONEXION"
 else
-    echo "Error al aplicar la configuración a $CONEXION" >&2
+    echo "Error al aplicar la configuración a la conexión $CONEXION" >&2
     exit 1
 fi
