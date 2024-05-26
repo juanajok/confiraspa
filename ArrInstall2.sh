a28945dd4df08ad359c0b5314b38ef97de6bd319
juanajok <juanajok@hotmail.com>
2024-05-26 17:41:55 +0200

    ok

--- ArrInstall2.sh
+++ ArrInstall2.sh
@@ -1,176 +1,156 @@
-
 #!/bin/bash
-### Description: \*Arr .NET Debian install
-### Originally written for Radarr by: DoctorArr - doctorarr@the-rowlands.co.uk on 2021-10-01 v1.0
-### Version v1.1 2021-10-02 - Bakerboy448 (Made more generic and conformant)
-### Version v1.1.1 2021-10-02 - DoctorArr (Spellcheck and boilerplate update)
-### Version v2.0.0 2021-10-09 - Bakerboy448 (Refactored and ensured script is generic. Added more variables.)
-### Version v2.0.1 2021-11-23 - brightghost (Fixed datadir step to use correct variables.)
-### Version v3.0.0 2022-02-03 - Bakerboy448 (Rewrote script to prompt for user/group and made generic for all \*Arrs)
-### Version v3.0.1 2022-02-05 - aeramor (typo fix line 179: 'chown "$app_uid":"$app_uid" -R "$bindir"' -> 'chown "$app_uid":"$app_guid" -R "$bindir"')
-### Version v3.0.3 2022-02-06 - Bakerboy448 fixup ownership
-### Version v3.0.3a Readarr to develop
-### Version v3.0.4 2022-03-01 - Add sleep before checking service status
-### Version v3.0.5 2022-04-03 - VP-EN (Added Whisparr)
-### Version v3.0.6 2022-04-26 - Bakerboy448 - binaries to group
-### Additional Updates by: The \*Arr Community
-
-### Boilerplate Warning
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-#NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-#LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-#OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-#WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-#constantes
-scriptversion="3.0.6"
-scriptdate="2022-04-26"
-INSTALL_BASE_DIR="/opt"
-DATA_BASE_DIR="/var/lib"
+### Description: *Arr .NET Debian install script
+### Version: 3.0.6
+### Last Updated: 2022-04-26
+### Authors: Bakerboy448, DoctorArr, brightghost, aeramor, VP-EN, *Arr Community
 
 set -euo pipefail
 
+# Constants
+SCRIPT_VERSION="3.0.6"
+SCRIPT_DATE="2022-04-26"
+INSTALL_BASE_DIR="/opt"
+DATA_BASE_DIR="/var/lib"
+
+# Logging function
 log() {
-    #función que permitirá que los mensajes de salida identifiquen al script, la función y fecha/hora de ejecución
     local message="$1"
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$0] [${FUNCNAME[1]}] $message"
 }
 
-
-
-function mostrar_info_version() {
-    log "Running *Arr Install Script - Version [$scriptversion] as of [$scriptdate]"
+# Show script version info
+show_version_info() {
+    log "Running *Arr Install Script - Version [$SCRIPT_VERSION] as of [$SCRIPT_DATE]"
 }
 
-function verificar_root() {
+# Verify if the script is running as root
+verify_root() {
     if [ "$EUID" -ne 0 ]; then
         log "Please run as root."
-        exit
+        exit 1
     fi
 }
 
-function obtener_usuario_grupo() {
+# Get user and group
+get_user_group() {
     app_uid=$(logname)
     app_guid=$(id -gn "$app_uid")
 }
 
-function crear_usuario_grupo() {
+# Create user and group if they do not exist
+create_user_group() {
     if [ "$app_guid" != "$app_uid" ]; then
         if ! getent group "$app_guid" >/dev/null; then
             groupadd "$app_guid"
+            log "Created group [$app_guid]"
         fi
     fi
     if ! getent passwd "$app_uid" >/dev/null; then
         adduser --system --no-create-home --ingroup "$app_guid" "$app_uid"
-        log "Created and added User [$app_uid] to Group [$app_guid]"
+        log "Created and added user [$app_uid] to group [$app_guid]"
     fi
     if ! getent group "$app_guid" | grep -qw "$app_uid"; then
-        log "User [$app_uid] did not exist in Group [$app_guid]"
         usermod -a -G "$app_guid" "$app_uid"
-        log "Added User [$app_uid] to Group [$app_guid]"
+        log "Added user [$app_uid] to group [$app_guid]"
     fi
 }
 
-function detener_aplicacion() {
+# Stop application if running
+stop_application() {
     local app=$1
-    if service --status-all | grep -Fq "$app"; then
-        systemctl stop $app
-        systemctl disable $app.service
-        log "Stopped existing $app"
+    if systemctl is-active --quiet "$app"; then
+        systemctl stop "$app"
+        systemctl disable "$app"
+        log "Stopped and disabled existing $app"
     fi
 }
 
-# Función para configurar directorios
-function configurar_directorios() {
+# Configure directories
+configure_directories() {
     local app_name=$1
-    local usuario=$2
-    local grupo=$3
+    local user=$2
+    local group=$3
 
     local install_dir="${INSTALL_BASE_DIR}/${app_name}"
-    log "La ruta de instaación es $install_dir"
     local data_dir="${DATA_BASE_DIR}/${app_name}"
 
-    if [ ! -d "${install_dir}" ]; then
-        mkdir -p "${install_dir}"
-        chown "${usuario}:${grupo}" "${install_dir}"
-        chmod 775 "${install_dir}"
-    fi
-
-    if [ ! -d "${data_dir}" ]; then
-        mkdir -p "${data_dir}"
-        chown "${usuario}:${grupo}" "${data_dir}"
-        chmod 775 "${data_dir}"
-    fi
-}
-
-
-# Función para verificar si un paquete está instalado
-function paquete_instalado() {
-    dpkg -s "$1" >/dev/null 2>&1
-}
-
-# Función para instalar paquetes requeridos
-function instalar_paquetes_requeridos() {
-    local paquetes=$1
-    for paquete in ${paquetes}; do
-        if ! paquete_instalado "${paquete}"; then
-            log "Instalando paquete: ${paquete}"
-            apt-get install -y "${paquete}"
+    for dir in "$install_dir" "$data_dir"; do
+        if [ ! -d "$dir" ]; then
+            mkdir -p "$dir"
+            chown "$user:$group" "$dir"
+            chmod 775 "$dir"
+            log "Created and set permissions for $dir"
         else
-            log "Paquete ya instalado: ${paquete}"
+            log "Directory $dir already exists"
         fi
     done
 }
 
-function descargar_instalar_aplicacion() {
+# Check if a package is installed
+is_package_installed() {
+    dpkg -s "$1" >/dev/null 2>&1
+}
+
+# Install required packages
+install_required_packages() {
+    local packages=$1
+    for package in $packages; do
+        if ! is_package_installed "$package"; then
+            log "Installing package: $package"
+            apt-get install -y "$package"
+        else
+            log "Package already installed: $package"
+        fi
+    done
+}
+
+# Download and install application
+download_install_application() {
     local app=$1
     local branch=$2
     local bindir=$3
     local datadir=$4
-    local app_uid=$5
-    local app_guid=$6
+    local user=$5
+    local group=$6
 
     ARCH=$(dpkg --print-architecture)
     dlbase="https://${app}.servarr.com/v1/update/${branch}/updatefile?os=linux&runtime=netcore"
 
     case "$ARCH" in
-    "amd64") DLURL="${dlbase}&arch=x64" ;;
-    "armhf") DLURL="${dlbase}&arch=arm" ;;
-    "arm64") DLURL="${dlbase}&arch=arm64" ;;
-    *)
-        log "Arch not supported"
-        exit 1
-        ;;
+        "amd64") DLURL="${dlbase}&arch=x64" ;;
+        "armhf") DLURL="${dlbase}&arch=arm" ;;
+        "arm64") DLURL="${dlbase}&arch=arm64" ;;
+        *)
+            log "Architecture not supported: $ARCH"
+            exit 1
+            ;;
     esac
 
-    log ""
-    log "Downloading..."
+    log "Downloading $app from $DLURL"
     wget --content-disposition "$DLURL"
-    tar -xvzf ${app^}.*.tar.gz
-    log ""
+    filename=$(ls ${app^}.*.tar.gz)
+    tar -xvzf "$filename"
     log "Installation files downloaded and extracted"
 
-    log "Removing existing installation"
-    rm -rf $bindir
-    log "Installing..."
-    mv "${app^}" $INSTALL_BASE_DIR
-    chown "$app_uid":"$app_guid" "$bindir"
-    chown -R "$app_uid":"$app_guid" "$bindir"
-    log "Application installed"
-    rm ${app^}.*.tar.gz
+    log "Removing existing installation in $bindir"
+    rm -rf "$bindir"
+    mv "${app^}" "$bindir"
+    chown -R "$user:$group" "$bindir"
+    chmod -R 755 "$bindir"
+    find "$bindir" -type f -name "${app^}" -exec chmod +x {} \;
+    log "$app installed to $bindir"
+
+    rm "$filename"
 }
 
-
-function configurar_servicio_systemd() {
+# Configure systemd service
+configure_systemd_service() {
     local app=$1
     local bindir=$2
     local datadir=$3
 
-    log ""
-    log "Creating systemd service"
-    cat >/etc/systemd/system/${app}.service <<EOL
+    log "Creating systemd service for $app"
+    cat >"/etc/systemd/system/${app}.service" <<EOL
 [Unit]
 Description=${app^} Daemon
 After=syslog.target network.target
@@ -179,57 +159,55 @@ After=syslog.target network.target
 User=${app_uid}
 Group=${app_guid}
 Type=simple
-ExecStart=${INSTALL_BASE_DIR}/${app^}/${app^} -nobrowser -data=${datadir}
+ExecStart=${bindir}/${app^} -nobrowser -data=${datadir}
 TimeoutStopSec=20
 KillMode=process
 Restart=on-failure
-Restart-sec=2
+RestartSec=2
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
-    log ""
-    log "Enabling and starting ${app^} service"
-    systemctl enable ${app}.service
-    systemctl start ${app}
+    log "Enabling and starting $app service"
+    systemctl enable "${app}.service"
+    systemctl start "${app}"
 }
 
-
-function iniciar_aplicacion() {
+# Start application
+start_application() {
     local app=$1
-    log "Starting ${app^} ..."
-    systemctl status $app
+    log "Starting $app"
+    systemctl status "$app"
 }
 
-# Función para instalar una aplicación específica
-function instalar_aplicacion() {
+# Install specific application
+install_application() {
     local app_name=$1
     local app_port=$2
     local app_prerequisites=$3
     local app_umask=$4
     local app_branch=$5
-    local usuario=$6
-    local grupo=$7
+    local user=$6
+    local group=$7
 
-    log "Instalando ${app_name}"
-    detener_aplicacion "${app_name}"
-    configurar_directorios "${app_name}" "${usuario}" "${grupo}"
-    instalar_paquetes_requeridos "${app_prerequisites}"
-    descargar_instalar_aplicacion "${app_name}" "${app_branch}" "${INSTALL_BASE_DIR}/${app_name^}" "${DATA_BASE_DIR}/${app_name}" "${usuario}" "${grupo}"
-    configurar_servicio_systemd "${app_name}" "${INSTALL_BASE_DIR}/${app_name}" "${DATA_BASE_DIR}/${app_name}"
-    iniciar_aplicacion "${app_name}"
+    log "Installing $app_name"
+    stop_application "$app_name"
+    configure_directories "$app_name" "$user" "$group"
+    install_required_packages "$app_prerequisites"
+    download_install_application "$app_name" "$app_branch" "${INSTALL_BASE_DIR}/${app_name^}" "${DATA_BASE_DIR}/${app_name}" "$user" "$group"
+    configure_systemd_service "$app_name" "${INSTALL_BASE_DIR}/${app_name}" "${DATA_BASE_DIR}/${app_name}"
+    start_application "$app_name"
 }
 
+# Main function
+main() {
+    show_version_info
+    verify_root
+    get_user_group
+    create_user_group
 
-
-function principal() {
-    mostrar_info_version
-    verificar_root
-    obtener_usuario_grupo
-    crear_usuario_grupo
-
-    # Leer el archivo JSON y procesar cada aplicación
+    # Read JSON file and process each application
     jq -c '.[]' apps.json | while read -r app_data; do
         app_name=$(jq -r '.name' <<< "$app_data")
         app_port=$(jq -r '.port' <<< "$app_data")
@@ -237,15 +215,10 @@ function principal() {
         app_umask=$(jq -r '.umask' <<< "$app_data")
         app_branch=$(jq -r '.branch' <<< "$app_data")
 
-        instalar_aplicacion "${app_name}" "${app_port}" "${app_prerequisites}" "${app_umask}" "${app_branch}" "${app_uid}" "${app_guid}"
+        install_application "$app_name" "$app_port" "$app_prerequisites" "$app_umask" "$app_branch" "$app_uid" "$app_guid"
     done
 
-    log ""
-    log ""
-    log "Script finalizado"
+    log "Script completed"
 }
 
-
-
-principal
-
+main
--- downloadclean.py
+++ downloadclean.py
@@ -1,23 +1,4 @@
-"""
-Script para procesar archivos descargados. 
-
-Si un archivo ya existe en la biblioteca, el script lo elimina de la ubicación original en la carpeta de descargas.
-
-Autor: [Juan José Hipólito]
-Fecha: [8 May 2023]
-
-Requerimientos:
-    - Python 3.5 o superior
-    - módulos de Python: os, shutil, json, hashlib, logging
-
-Instrucciones de uso:
-    - Editar el archivo 'directories.json' con los directorios correctos de descarga y bibliotecas de medios.
-    - Ejecutar el script 'downloadclean.py'.
-
-"""
-
 import os
-import shutil
 import json
 import hashlib
 import logging
@@ -27,35 +8,15 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s [%(filename)s:%(funcName)s]: %(message)s', 
     level=logging.INFO,
     handlers=[
-        logging.StreamHandler(), #para que los mensajes del logger se muestren en la consola
-        logging.FileHandler('log.txt') #para que los mensajes se guarden en un archivo de registro
+        logging.StreamHandler(),  # Para que los mensajes del logger se muestren en la consola
+        logging.FileHandler('log.txt')  # Para que los mensajes se guarden en un archivo de registro
     ]
 )
 
 logger = logging.getLogger()
 
-# Configurar los niveles de los handlers
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-file_handler = logging.FileHandler('log.txt')
-file_handler.setLevel(logging.WARNING)
-
-# Agregar los handlers al logger
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
-
-
-  
 def get_hash(file_path):
-    """
-    Calcula y devuelve el hash MD5 de un archivo en bytes.
-    
-    Parámetros:
-        - file_path (str): ruta al archivo que se va a calcular el hash.
-        
-    Devuelve:
-        - str: el hash MD5 del archivo o None si no se puede abrir o leer el archivo.
-    """
+    """Calcula y devuelve el hash MD5 de un archivo en bytes."""
     hasher = hashlib.md5()
     try:
         with open(file_path, 'rb') as f:
@@ -66,53 +27,29 @@ def get_hash(file_path):
         logger.error(f"Error while reading file {file_path}: {e}")
         return None
 
-
-
 def check_file_in_library(file_path, library_dir):
-    """
-    Comprueba si un archivo existe en una biblioteca de medios mediante su hash MD5.
-
-    Parámetros:
-        - file_path (str): ruta al archivo que se va a verificar.
-        - library_dir (str): ruta al directorio de la biblioteca de medios.
-
-    Devuelve:
-        - bool: True si el archivo existe en la biblioteca, False en caso contrario.
-    """
+    """Comprueba si un archivo existe en una biblioteca de medios mediante su hash MD5."""
     file_size = os.path.getsize(file_path)
     logger.debug(f"Checking if file {file_path} with size {file_size} is in {library_dir}")
+    
     for entry in os.scandir(library_dir):
         if entry.is_file() and entry.stat().st_size == file_size:
             file_hash = get_hash(file_path)
-            if file_hash is not None:
-                if get_hash(entry.path) == file_hash:
-                    logger.info(f"File {file_path} found in {library_dir}")
-                    return True
-                else:
-                    logger.debug(f"Hashes for file {file_path} and {entry.path} do not match")
-            else:
+            if file_hash and get_hash(entry.path) == file_hash:
+                logger.info(f"File {file_path} matches with {entry.path} in {library_dir}")
+                return True
+            elif not file_hash:
                 logger.debug(f"Could not calculate hash for file {file_path}")
         elif entry.is_dir():
             logger.debug(f"Checking directory {entry.path}")
             if check_file_in_library(file_path, entry.path):
                 return True
+    
     logger.debug(f"File {file_path} not found in {library_dir}")
     return False
 
-
-
-
-
 def delete_file(file_path):
-    """
-    Borra un archivo.
-
-    Args:
-        - file_path (str): La ruta al archivo que se va a borrar.
-
-    Returns:
-        - (bool) True si el archivo se borró correctamente, False en caso contrario.
-    """
+    """Borra un archivo."""
     try:
         os.unlink(file_path)
         logger.info(f"Deleted file {file_path}")
@@ -124,123 +61,82 @@ def delete_file(file_path):
         logger.error(f"Error deleting {file_path}: {e}")
         return False
 
-    
 def get_file_extension(file_path):
-    """
-    Obtiene la extensión de un archivo.
-
-    Args:
-    - file_path (str): La ruta al archivo.
-
-    Returns:
-    - (str) La extensión del archivo en minúsculas, incluyendo el punto inicial (por ejemplo, ".txt").
-    """
+    """Obtiene la extensión de un archivo."""
     return os.path.splitext(file_path)[1].lower()
 
 def find_files_recursively(directory, extensions):
-    """
-    Recursively finds all files with given extensions in a directory and its subdirectories.
-
-    Parameters:
-        - directory (str): path to the directory to search in.
-        - extensions (list of str): list of file extensions to search for (including the dot).
-
-    Returns:
-        - list of str: list of file paths that match the given extensions.
-    """
+    """Encuentra recursivamente todos los archivos con las extensiones dadas en un directorio y sus subdirectorios."""
     matching_files = []
-
+    
     try:
-        entries = os.scandir(directory)
-    except FileNotFoundError:
-        logger.warning(f"Directory not found: {directory}")
-        return matching_files
-    except PermissionError:
-        logger.warning(f"Permission denied for directory: {directory}")
-        return matching_files
-
-    for entry in entries:
-        if os.path.isfile(entry):
-            extension = get_file_extension(entry)
-            if extension in extensions:
-                matching_files.append(entry)
-            else:
-                logger.debug(f"Skipping file {entry} with extension {extension}")
-        elif entry.is_dir():
-            logger.debug(f"Recursing into directory {entry.path}")
-            matching_files += find_files_recursively(entry.path, extensions)
-        else:
-            logger.debug(f"Skipping non-file/directory {entry}")
-
+        for entry in os.scandir(directory):
+            if entry.is_file() and get_file_extension(entry.path) in extensions:
+                logger.info(f"Found file {entry.path} with extension {get_file_extension(entry.path)}")
+                matching_files.append(entry.path)
+            elif entry.is_dir():
+                logger.debug(f"Recursing into directory {entry.path}")
+                matching_files += find_files_recursively(entry.path, extensions)
+    except (FileNotFoundError, PermissionError) as e:
+        logger.warning(f"Error accessing directory {directory}: {e}")
+    
     return matching_files
 
 def remove_empty_directories_recursively(top_directory):
-    """
-    Recursively removes all empty directories under the given top directory.
-
-    Parameters:
-        - top_directory (str): path to the top directory to search in.
-
-    Returns:
-        - None
-    """
+    """Elimina recursivamente todos los directorios vacíos bajo el directorio superior dado."""
     for root, dirs, _ in os.walk(top_directory, topdown=False):
         for directory in dirs:
             directory_path = os.path.join(root, directory)
-            if not os.listdir(directory_path): # Check if directory is empty
+            if not os.listdir(directory_path):  # Verifica si el directorio está vacío
                 os.rmdir(directory_path)
-                print(f"Directorio vacío {directory_path} borrado")
-
+                logger.info(f"Deleted empty directory {directory_path}")
 
 def main():
-    # Load directories from JSON
+    # Cargar directorios desde JSON
     config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "directories.json")
-    with open(config_path, "r") as f:
-        directories = json.load(f)
+    try:
+        with open(config_path, "r") as f:
+            directories = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        logger.error(f"Error loading configuration file: {e}")
+        return
 
     download_dir = directories.get("download_dir")
     series_dir = directories.get("series_dir")
     movies_dir = directories.get("movies_dir")
     music_dir = directories.get("music_dir")
 
-    # Check if all required directories are present in the JSON file
+    # Verifica si todos los directorios necesarios están presentes en el archivo JSON
     if None in [download_dir, series_dir, movies_dir, music_dir]:
         logger.error("One or more directories are missing from directories.json. Please check the file and try again.")
         return
 
-    # Define the list of file types and their associated information
+    # Define la lista de tipos de archivo y su información asociada
     file_types = [
         ((".mp4", ".mkv", ".avi"), series_dir, "Series", "series"),
         ((".mp4", ".mkv", ".avi"), movies_dir, "Movies", "movies"),
         ((".mp3", ".flac", ".ogg"), music_dir, "Music", "music")
     ]
 
-    # Get the file extensions for each file type
-    extensions = {}
-    for extensions_list, _, _, print_name in file_types:
-        extensions[print_name] = [extension.lower() for extension in extensions_list]
-
-    # Iterate over the file types and process the files
+    # Itera sobre los tipos de archivo y procesa los archivos
     for extensions, destination_dir, log_name, print_name in file_types:
         matching_files = find_files_recursively(download_dir, extensions)
         if matching_files:
             logger.info(f"Processing {len(matching_files)} {print_name} files")
             for file_path in matching_files:
+                logger.info(f"Processing file {file_path}")
                 if check_file_in_library(file_path, destination_dir):
                     if delete_file(file_path):
-                        logger.info(f"{file_path} deleted from {print_name} library")
+                        logger.info(f"Deleted {file_path} from {print_name} library")
                     else:
                         logger.warning(f"Failed to delete {file_path} from {print_name} library")
+                else:
+                    logger.info(f"File {file_path} not found in {print_name} library, skipping deletion.")
         else:
             logger.info(f"No {print_name} files found to process")
 
-    # delete empty folders
-
+    # Elimina carpetas vacías
     remove_empty_directories_recursively(download_dir)
-
-    # Clean up logging handlers
-    logger.handlers = []
-
 
 if __name__ == '__main__':
     main()
