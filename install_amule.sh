c8130ece34d43d198a3b64c3653da9e6a18187ad
juanajok <juanajok@hotmail.com>
2024-05-27 18:36:48 +0200

    se corrige install_amule para que sea idempotente

--- install_amule.sh
+++ install_amule.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+# Variables necesarias
+usuario="$SUDO_USER"
+script_path="$(pwd)"
+credenciales_json="$script_path/credenciales.json"
+directories_json="$script_path/amule_directories.json"
+
 # Función de registro para imprimir mensajes con marca de tiempo
 log() {
     local message="$1"
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] [${FUNCNAME[1]}] $message"
 }
+
+# Verifica que el archivo de credenciales existe
+if [ ! -f "$credenciales_json" ]; then
+    log "Error: El archivo de credenciales $credenciales_json no se encuentra."
+    exit 1
+fi
+
+# Lee la contraseña desde el archivo JSON
+contrasena=$(jq -r '.password' "$credenciales_json")
+
+# Verifica que el archivo de directorios existe
+if [ ! -f "$directories_json" ]; then
+    log "Error: El archivo de directorios $directories_json no se encuentra."
+    exit 1
+fi
 
 # Imprime un mensaje indicando que se están instalando aMule y sus dependencias
 log "Instalando aMule y sus dependencias..."
@@ -16,26 +37,27 @@ apt-get install -y amule amule-utils amule-daemon amule-utils-gui
 log "aMule y sus dependencias instaladas correctamente."
 
 # Ejecuta aMule por primera vez para generar archivos de configuración
-sudo -u $usuario amuled --ec-config <<< "$contrasena" &
-
-# Espera 20 segundos y luego detiene el servicio de aMule
+sudo -u $usuario amuled &
 sleep 20
 pkill -f amuled
 log "El demonio de aMule ha sido detenido."
 
-cd "$script_path"
+# Verifica que el archivo de configuración existe
+amule_conf_path="/home/$usuario/.aMule/amule.conf"
+if [ ! -f "$amule_conf_path" ]; then
+    log "Error: El archivo de configuración $amule_conf_path no se encuentra."
+    exit 1
+fi
 
 # Crea una copia de seguridad del archivo de configuración de aMule
-cp /home/$usuario/.aMule/amule.conf /home/$usuario/.aMule/amule.conf.backup
+cp "$amule_conf_path" "$amule_conf_path.backup"
 log "Copia de seguridad de amule.conf creada."
 
 # Lee las rutas de directorios del archivo JSON
-directories_json="amule_directories.json"
 incoming_directory=$(jq -r '.incoming_directory' "$directories_json")
 temp_directory=$(jq -r '.temp_directory' "$directories_json")
 
 # Actualiza el archivo de configuración de aMule con las nuevas rutas
-amule_conf_path="/home/$usuario/.aMule/amule.conf"
 sed -i "s|^IncomingDir=.*$|IncomingDir=$incoming_directory|" "$amule_conf_path"
 sed -i "s|^TempDir=.*$|TempDir=$temp_directory|" "$amule_conf_path"
 sed -i "s|^Template=.*$|Template=webserver|" "$amule_conf_path"
